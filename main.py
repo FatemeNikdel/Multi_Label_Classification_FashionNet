4a2bac37ee3a38ac3f2058d420c54731fd491684
FatemeNikdel <nikdel_fateme@yahoo.com>
2023-01-18 11:59:08 +0330

    change the name of module

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 import deep_net
 import utility
 import tensorflow as tf
-from sklearn.model_selection import train_test_split
-import cv2
-import glob
-import numpy as np
-from sklearn.preprocessing import LabelBinarizer
-
+import data_preprocessing
 
 path = r"FashionNet\clothes_dataset\dataset"
 
