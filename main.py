55d1b29f7699deb970ff8b69218b25315290a0bd
FatemeNikdel <nikdel_fateme@yahoo.com>
2023-01-18 12:07:45 +0330

    load data

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import deep_net
 import utility
 import tensorflow as tf
-import data_preprocessing
+from data_preprocessing import PreProcessing
 
 path = r"FashionNet\clothes_dataset\dataset"
 
+prep = PreProcessing()
+data, category_label, color_label = prep.load_data()
+category_label, color_label = prep.label_binarizer(category_label, color_label)
+X_train, X_test, Y_train_category, Y_test_category, Y_train_color, Y_test_color = prep.train_test_split(data,
+                                                                                    category_label, color_label)
 
 
 
