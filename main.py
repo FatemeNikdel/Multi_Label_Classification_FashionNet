c768da95e07c597672b1164466cfe09872bec673
FatemeNikdel <nikdel_fateme@yahoo.com>
2023-01-18 11:04:00 +0330

    define preprocessing module

--- main.py
+++ main.py
@@ -5,24 +5,12 @@ from sklearn.model_selection import train_test_split
 import cv2
 import glob
 import numpy as np
+from sklearn.preprocessing import LabelBinarizer
 
 
 path = r"FashionNet\clothes_dataset\dataset"
-data   = []
-labels = []
-def load_data(path):
-    for i , name in enumerate(glob.glob(path + "\\*\\*")):
-        # Read images
-        img = cv2.imread(name)
-        # Resize and Normalize
-        img = cv2.resize(img, (64, 64))/255.0
-        # Create Dataset
-        data.append(img)
-        # Create Labels
-        label = name.split("\\")[-2]
-        labels.append(label)
 
-def Train_Test_split():
-    
+
+
 
 
