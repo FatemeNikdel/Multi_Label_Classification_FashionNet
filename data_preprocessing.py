04d4a97cb392b1850363f11ba3e15d43e4e82ea6
FatemeNikdel <nikdel_fateme@yahoo.com>
2023-02-14 15:20:21 +0330

    edit the deep net

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -6,44 +6,50 @@ import numpy as np
 from sklearn.preprocessing import LabelBinarizer
 
 class PreProcessing():
-
+    # Define the constructor method with a parameter "path"
     def __init__(self, path):
         self.path = path
-
+    # Define a method named "load_data"
     def load_data(self):
+        # Create empty lists to hold data and labels
         data   = []
         category_label = []
         color_label = []
+        # Loop through each image in the directory specified by the path
         for i , name in enumerate(glob.glob(self.path + "\\*\\*")):
             # Read images
             img = cv2.imread(name)
             # Resize and Normalize
             img = cv2.resize(img, (96, 96))/255.0
             # RGB Color
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            #img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             # Create Dataset
             data.append(img)
             # Create Labels
             category_label.append(name.split("\\")[-2].split('_')[1]) 
             color_label.append(name.split("\\")[-2].split('_')[0])
-
+            # Print progress information for every 100 images processed
             if i % 100 == 0:
-                print(f"[INFO]: {i}/25000 processed!")
+                print(f"[INFO]: {i}/2500 processed!")
             
-            return data, category_label, color_label
-
-
-    def label_binarizer(category_label, color_label):
+        return data, category_label, color_label
+        
+    # Define a method named "label_binarizer"
+    def label_binarizer(self, category_label, color_label):
         color_label = np.array(color_label)
         category_label = np.array(category_label)
+        # Create instances of the LabelBinarizer class for both the category and color labels
         category_LB = LabelBinarizer()
         Color_LB = LabelBinarizer()
+        # Transform the category and color labels into binary arrays
         category_label = category_LB.fit_transform(category_label)
-        color_label = category_LB.fit_transform(color_label)
+        color_label = Color_LB.fit_transform(color_label)
         
         return category_label, color_label
 
-    def train_test_split(data, category_label, color_label):
+    # Define a method named "train_test_split"
+    def train_test_split(self, data, category_label, color_label):
+        # Split the dataset and labels into training and testing sets
         split = train_test_split(data, category_label, color_label, test_size = 0.2 )
         X_train, X_test, Y_train_category, Y_test_category, Y_train_color, Y_test_color = split
 
--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-import deep_net
+from deep_net import FashionNet
 import utility
 import tensorflow as tf
 from data_preprocessing import PreProcessing
 
-path = r"FashionNet\clothes_dataset\dataset"
+path   = r"FashionNet\clothes_dataset\dataset"
+epochs = 50
 
-prep = PreProcessing()
+prep = PreProcessing(path)
 data, category_label, color_label = prep.load_data()
 category_label, color_label = prep.label_binarizer(category_label, color_label)
 X_train, X_test, Y_train_category, Y_test_category, Y_train_color, Y_test_color = prep.train_test_split(data,
                                                                                     category_label, color_label)
 
+model = FashionNet(X_train, X_test, Y_train_category, Y_test_category, Y_train_color, Y_test_color, epochs)
+net = model.build()
 
 
 
